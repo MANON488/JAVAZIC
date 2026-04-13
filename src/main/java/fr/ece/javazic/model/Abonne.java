21296f013b7917f492aca9b90847daabd1e5cadc
MANON488 <150056901+MANON488@users.noreply.github.com>
2026-04-13 23:24:15 +0200

    Update Abonne.java

--- src/main/java/fr/ece/javazic/model/Abonne.java
+++ src/main/java/fr/ece/javazic/model/Abonne.java
@@ -24,16 +24,45 @@ public class Abonne extends Visiteur {
         return getEmail().equals(email) && getMotDePasse().equals(mdp);
     }
 
-    @Override
-    public void ecouter(Morceau m) { /* TODO (Man) */ }
-    public Playlist creerPlaylist(String nom) { /* TODO (Man) */ return null; }
-    public void supprimerPlaylist(Playlist p) { /* TODO (Man) */ }
-    public void noter(Morceau m, int valeur, String commentaire) { /* TODO (Man) */ }
-    public void modifierNote(Note n, int valeur, String commentaire) { /* TODO (Man) */ }
-    public void supprimerNote(Note n) { /* TODO (Man) */ }
+@Override
+public void ecouter(Morceau m) {
+    if (m != null) { m.incrementerEcoutes(); historique.ajouterEcoute(new Ecoute(m));System.out.println("Lecture du morceau : " + m.getTitre()); }
+}
+
+public Playlist creerPlaylist(String nom) {
+    Playlist playlist = new Playlist(nom);playlists.add(playlist);return playlist;
+}
+
+public void supprimerPlaylist(Playlist p) {
+    playlists.remove(p);
+}
+
+public void noter(Morceau m, int valeur, String commentaire) {
+    if (m != null) { Note note = new Note(m, valeur, commentaire);notes.add(note); }
+}
+
+public void modifierNote(Note n, int valeur, String commentaire) {
+    if (n != null) { n.setValeur(valeur); n.setCommentaire(commentaire);}
+}
+
+public void supprimerNote(Note n) {
+    notes.remove(n);
+}
 
     @Override
-    public void afficherMenu() { /* TODO (Man) */ }
+    public void afficherMenu() {
+        System.out.println("\n..... MENU ABONNE .....");
+        System.out.println("1. Consulter le catalogue");
+        System.out.println("2. Rechercher un morceau");
+        System.out.println("3. Creer une playlist");
+        System.out.println("4. Voir mes playlists");
+        System.out.println("5. Supprimer une playlist");
+        System.out.println("6. Consulter l'historique");
+        System.out.println("7. Noter un morceau");
+        System.out.println("8. Modifier une note");
+        System.out.println("9. Supprimer une note");
+        System.out.println("10. Quitter");
+    }
 
     public boolean isSuspendu() { return suspendu; }
     public void setSuspendu(boolean suspendu) { this.suspendu = suspendu; }
